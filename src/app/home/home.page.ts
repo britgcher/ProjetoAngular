5d080f9b7e539ab2a432e460001935196f35a751
britgcher <bn922084@gmail.com>
2023-10-04 16:27:04 -0300

    45555555

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,116 +1,207 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+// Importando módulos necessários
+import { CommonModule } from '@angular/common'; // Módulo comum do Angular
+import { Component } from '@angular/core'; // Componente do Angular
+import { IonicModule } from '@ionic/angular'; // Módulo de UI do Ionic
+import { AlertController } from '@ionic/angular';
 
 @Component({
+  // Nome do seletor HTML para este componente
   selector: 'app-home',
+
+  // Caminho para o arquivo HTML do componente
   templateUrl: 'home.page.html',
+
+  // Estilos CSS específicos para este componente
   styleUrls: ['home.page.scss'],
+
+  // Define se este componente é independente (standalone)
   standalone: true,
+
+  // Importação de módulos necessários
   imports: [IonicModule, CommonModule],
-
 })
+
 export class HomePage {
-  isLoading: boolean = false;
-  funcionarios: any;
-  codigofuncionario: any;
-
-
-  public alertButtons = [
-    {
-      text: 'Cancelar',
-      role: 'cancel',
-      handler: () => {
-        console.log('Alert canceled');
-      },
-    },
-    {
-      text: 'OK',
-      role: 'confirm',
-      handler: () => {
-        this.remover(this.codigofuncionario)
-      },
-    },
-  ];
-
-  setCodigo(codigo:any){ 
-    this.codigofuncionario = codigo
-  }
-
-
-  constructor(){
-    this.getFuncionarios();
-  }
-
-  getFuncionarios(){
-    this.isLoading = true;
-    fetch('http://localhost/xampp/exercicio/funcionario/listar_funcionario.php')
-    .then(response => response.json())
-    .then(response => {
-      this.funcionarios=response['funcionarios'];
-    })
-    .catch(erro => {
-      console.log(erro);
-    })
-    .finally(()=>{
-      this.isLoading = false;
-    })
-  }
-
-
+  // Propriedades do componente
+  isLoading: boolean = false; // Indicador de carregamento
+  funcionarios: any; // Lista de funcionários
+  codigoFuncionario: any; // Código do funcionário selecionado
+  modalAtualizar = false; // Indicador de abertura do modal
   
-  remover(funcionario:any){
-    this.isLoading = true;
-    fetch('http://localhost/xampp/exercicio/funcionario/remover_funcionario.php',
-			{
-			  method: 'POST',
-			  headers: {
-			    'Content-Type': 'application/json',
-			  },
-			  body: JSON.stringify(funcionario)
-			}
-		)
+
+    // Construtor do componente
+    constructor(public alertController: AlertController) {
+      this.listarFuncionarios(); // Inicia a listagem de funcionários
+    }
+
+     // Função para definir o código do funcionário selecionado
+  setCodigo(codigo: any) {
+    this.codigoFuncionario = codigo;
+  }
+
+ // Função para listar funcionários
+ listarFuncionarios() {
+  this.isLoading = true; // Indica que está carregando
+  fetch('http://localhost/xampp/exercicio/funcionario/listar_funcionario.php')
     .then(response => response.json())
     .then(response => {
-      console.log(response["mensagem"]);
-      this.getFuncionarios();
+      this.funcionarios = response['funcionarios']; // Define a lista de funcionários
     })
     .catch(erro => {
       console.log(erro);
     })
-    .finally(()=>{
-      this.isLoading = false;
+    .finally(() => {
+      this.isLoading = false; // Indica que terminou o carregamento
+    });
+}
+  
+//////////////////////////////////////////////////////////////////
+  
+  //Excluir Funcionario
+
+  // Função para remover um funcionário
+  remover(funcionario: any) {
+    this.isLoading = true;
+    
+    fetch('http://localhost/xampp/exercicio/funcionario/remover_funcionario.php', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(funcionario),
     })
+    .then(response => response.json())
+    .then(response => {
+      console.log(response[":)"]);
+      this.listarFuncionarios();
+    })
+    .catch(erro => {
+      console.log(erro);
+    })
+    .finally(() => {
+      this.isLoading = false;
+    });
   }
 
-  atualizar(codigo:any){
+
+  //interligar
+  async exibirAlertaExclusao(codigoFuncionario: any) {
+    const alert = await this.alertController.create({
+      header: 'Confirmação',
+      message: 'Deseja excluir esse funcionário?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Exclusão cancelada');
+          }
+        }, {
+          text: 'OK',
+          handler: () => {
+            this.remover(codigoFuncionario);
+          }
+        }
+      ]
+    });
+  
+    await alert.present();
+  }
+  
+  /////////////////////////////////////////////////////////////////
+  
+  // Atualizar Funcionario
+
+  // Função para atualizar um funcionário
+  atualizar(codigo: any, novoNome: string, novoCargo: string, novaCidade: string, novoFone: string) {
     this.isLoading = true;
+    
     fetch('http://localhost/xampp/exercicio/funcionario/atualizar_funcionario.php',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ CodFun: codigo})
-      }
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ CodFun: codigo, Nome: novoNome, Cargo: novoCargo, Cidade: novaCidade, Fone: novoFone }),
+        }
     )
     .then(response => response.json())
     .then(response => {
-      console.log(response["mensagem"]);
-      this.getFuncionarios();
+        console.log(response);
+        this.listarFuncionarios();
     })
     .catch(erro => {
-      console.log(erro);
+        console.log(erro);
     })
-    .finally(()=>{
-      this.isLoading = false;
-    })
-  }
-
- 
+    .finally(() => {
+        this.isLoading = false;
+    });
+}
 
 
+ // Função para abrir ou fechar o modal
+ abrirModalAtualizar(isOpen: boolean) {
+  this.modalAtualizar = isOpen;
 }
 
 
 
+  //interligar
+  async exibirAlertaAtualizar(codigoFuncionario: any) {
+    const alert = await this.alertController.create({
+      header: 'Confirmação',
+      message: 'Deseja excluir esse funcionário?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Exclusão cancelada');
+          }
+        }, {
+          text: 'OK',
+          handler: () => {
+            this.remover(codigoFuncionario);
+          }
+        }
+      ]
+    });
+  
+    await alert.present();
+  }
+
+
+
+
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+ 
+
+  
+  
+ 
+
+
+  
+ 
+
+
+ 
+ 
+
+  
+}
+  
